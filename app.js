525d7d2cc8eacdf0af1f494bf8f3ade666feabc5
jyothika28 <jyothikajalla032@gmail.com>
2023-12-04 13:29:04 -0500

    Added mongo string

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import session from "express-session";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+//const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect('mongodb+srv://Jyothika:Skyhigh100@cluster0.cgj2l7l.mongodb.net/kanbas?retryWrites=true&w=majority');
+//mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express();
 app.use(cors({
