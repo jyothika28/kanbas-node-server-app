87745ddac3a26a1e30957a65bbda4f66b61ac4fa
jyothika28 <jyothikajalla032@gmail.com>
2023-12-03 22:22:59 -0500

    A6 node commit

--- app.js
+++ app.js
@@ -5,11 +5,36 @@ import cors from "cors";
 import "dotenv/config";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }
+ ));
+ 
+ const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+  
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
